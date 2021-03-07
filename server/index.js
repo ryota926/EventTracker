90066bfcd5468a582bd3f38a640508573477ca71
ryota926 <ryota.suzuki7@gmail.com>
2021-03-06 22:55:56 -0800

    Added backend setup

---
+++ server/index.js
@@ -0,0 +1,6 @@
+import express from 'express' //framework for creating routing
+import bodyParser from 'body-parser' //enable post request
+import mongoose from 'mongoose' //create models for our post
+import cors from 'cors' //enable cross origin request
+
+
