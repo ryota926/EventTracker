45c5eda6f0ea4d4d62841048ac335adaf90df560
ryota926 <ryota.suzuki7@gmail.com>
2021-03-06 23:40:18 -0800

    Added backend folder structure

---
+++ server/models/post.Message.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const postSchema = mongoose.Schema({
+    title: String,
+    message: String,
+    author: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    postedAt: {
+        type: Date,
+        default: new Date()
+    }
+
+
+})
+
+const PostMessage = mongoose.model('PostMessage', postSchema) //turn schema into model
+
+
+export default PostMessage; //exporting mongoose model
