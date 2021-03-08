af62390f3693dfb74aad10620c165c5dd496f845
ryota926 <ryota.suzuki7@gmail.com>
2021-03-07 18:42:05 -0800

    Added Redux stuffs

--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ import postRoutes from './routes/posts.js'
 
 const app = express() //initialize app
 
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
+
+app.use('/posts', postRoutes)
+
 
 const CONNECTION_URL = 'mongodb+srv://ryota:bdEJjrOFQwrYFCT0@cluster0.q6p9g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
