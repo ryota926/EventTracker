7287c137759567a6b8075d08d615b40cc58348f6
ryota926 <ryota.suzuki7@gmail.com>
2021-03-09 00:23:08 -0800

    Added constants and env files

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ import express from 'express' //framework for creating routing
 import bodyParser from 'body-parser' //enable post request
 import mongoose from 'mongoose' //create models for our post
 import cors from 'cors' //enable cross origin request
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/posts.js'
-
+dotenv.config()
 
 const app = express() //initialize app
 
@@ -15,11 +16,9 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-
-const CONNECTION_URL = 'mongodb+srv://ryota:bdEJjrOFQwrYFCT0@cluster0.q6p9g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message))
 
