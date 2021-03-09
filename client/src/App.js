a21ff5cbd9b615b91782e147e68ba8a3e1358b18
ryota926 <ryota.suzuki7@gmail.com>
2021-03-08 23:54:12 -0800

    Added mobile compatibility

--- client/src/App.js
+++ client/src/App.js
@@ -21,14 +21,13 @@ const App = () => {
 
     return(
         <Container maxWidth="lg"> 
-        <AppBar className={classes.appBar} position="static" color="inherit">
+            <AppBar className={classes.appBar} position="static" color="inherit">
                 <Typography className = {classes.heading} variant = "h2" align = "center"> Memories </Typography>
-
                 <img className = {classes.image} src = {HighLights} alt = "HighLights" height = "60"/>
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify = "space-between" alignItems = "stretch" spacing = {3}> 
+                    <Grid className = {classes.mainContainer} container justify = "space-between" alignItems = "stretch" spacing = {3}> 
                         <Grid item xs = {12} sm={7}>
                             <Posts setCurrentId = {setCurrentId} />
                         </Grid>
